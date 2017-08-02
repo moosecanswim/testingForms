a8abfffbe893d6ab0cda06674a520c6f62d8332b
moosecanswim <kyle.gallagher.09@gmail.com>
2017-08-02 11:51:15 -0400

    FirstCommit.  nothing is entered this is just a base

---
+++ src/main/java/me/moosecanswim/testingforms/TestingformsApplication.java
@@ -0,0 +1,12 @@
+package me.moosecanswim.testingforms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingformsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingformsApplication.class, args);
+	}
+}
